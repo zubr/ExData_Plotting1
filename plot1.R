0de0f5584781f282fa60596b42522202421670e0
zubr <zuzkabe@poczta.fm>
2014-12-02 19:46:28 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -3,3 +3,8 @@ dat <- subset(data, Date=='1/2/2007' | Date=='2/2/2007')
 png("plot1.png", width = 480, height = 480)
 hist(as.numeric(as.character(dat$Global_active_power)), xlab="Global Active Power (kilowatts)", col="red", ylab="Frequency", main="Global Active Power", cex.lab=0.8, cex.axis=0.8, cex.main=0.8)
 dev.off()
+
+#The plot itself may be viewed after clicking on https://github.com/zubr/ExData_Plotting1
+#then click on "... commits" and the plot is attached as a comment to commit "Update plot1.R"
+#Sorry for that but I cannot install github on my pc (windows XP) and there seem to be no
+#other option to download a chart on github (i.e. without cloning the repo in desktop).
